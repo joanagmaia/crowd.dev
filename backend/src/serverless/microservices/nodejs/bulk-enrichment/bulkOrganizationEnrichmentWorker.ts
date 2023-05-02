e7b0b6145b90255ed054a7552f541bebc1e41a4a
Ighor Jesse <ighor.jesse@gmail.com>
2023-05-02 18:48:32 +0100

    improv: refactored worker

--- backend/src/serverless/microservices/nodejs/bulk-enrichment/bulkOrganizationEnrichmentWorker.ts
+++ backend/src/serverless/microservices/nodejs/bulk-enrichment/bulkOrganizationEnrichmentWorker.ts
@@ -12,47 +12,28 @@ async function BulkorganizationEnrichmentWorker(tenantId: string) {
   const enrichmentLimit =
     PLAN_LIMITS[userContext.currentTenant.plan][FeatureFlag.ORGANIZATION_ENRICHMENT]
 
+  const enrichmentService = new OrganizationEnrichmentService({
+    options: userContext,
+    apiKey: ORGANIZATION_ENRICHMENT_CONFIG.apiKey,
+    tenantId: userContext.currentTenant.id,
+    limit: enrichmentLimit,
+  })
+  const successfulEnrichments = await enrichmentService.enrichOrganizationsAndSignalDone()
+
   const redis = await createRedisClient(true)
   const enrichmentCountCache = new RedisCache(
     FeatureFlagRedisKey.ORGANIZATION_ENRICHMENT_COUNT,
     redis,
   )
-  enrichmentCountCache.setValueIfNotExistsAlready(
-    userContext.currentTenant.id,
-    `${enrichmentLimit}`,
-  )
-
-  let unusedEnrichmentCount = parseInt(
-    await enrichmentCountCache.getValue(userContext.currentTenant.id),
-    10,
-  )
-  const enrichmentService = new OrganizationEnrichmentService({
-    options: userContext,
-    apiKey: ORGANIZATION_ENRICHMENT_CONFIG.apiKey,
-    tenantId: userContext.currentTenant.id,
-    limit: unusedEnrichmentCount,
-  })
-  const successfulEnrichments = await enrichmentService.enrichOrganizationsAndSignalDone()
-  unusedEnrichmentCount -= successfulEnrichments.length
 
   const secondsRemainingUntilEndOfMonth = getSecondsTillEndOfMonth()
+  await enrichmentCountCache.setValue(
+    userContext.currentTenant.id,
+    (successfulEnrichments.length).toString(),
+    secondsRemainingUntilEndOfMonth,
+  )
 
-  if (!unusedEnrichmentCount) {
-    await enrichmentCountCache.setValue(
-      userContext.currentTenant.id,
-      '0',
-      secondsRemainingUntilEndOfMonth,
-    )
-  } else {
-    // Before sending the queue message, we increase the memberEnrichmentCount with all member Ids that are sent,
-    // assuming that we'll be able to enrich all.
-    // If any of enrichments failed, we should add these credits back, reducing memberEnrichmentCount
-    await enrichmentCountCache.setValue(
-      userContext.currentTenant.id,
-      (unusedEnrichmentCount + enrichmentLimit).toString(),
-      secondsRemainingUntilEndOfMonth,
-    )
-  }
+
 }
 
 export { BulkorganizationEnrichmentWorker }

804063ede562f4530bcb9161dfc867e384d89846
Joana Maia <joana@crowd.dev>
2023-05-19 18:37:51 +0100

    Revert cronTime

--- backend/src/bin/jobs/organizationEnricher.ts
+++ backend/src/bin/jobs/organizationEnricher.ts
@@ -9,7 +9,7 @@ import { getServiceLogger } from '../../utils/logging'
 
 const job: CrowdJob = {
   name: 'organization enricher',
-  cronTime: cronGenerator.everyHourAt(55),
+  cronTime: cronGenerator.everyDay(),
   onTrigger: sendWorkerMessage,
 }
 
